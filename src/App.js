ef52775da3370ddd5ccc1d919829ab1aeb83ab65
Chandana Venkatadri <chandanavenkatadri@Chandanas-MacBook-Air.local>
2024-04-01 20:28:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import './App.css'; // Import your CSS file for styling
 
 function App() {
+  const responseGoogle = (response) => {
+    console.log(response);
+    // Handle Google login response
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <GoogleLogin
+        clientId="721750587677-gl0q7r4el0aasj5ka8vmsmt7fpb528on.apps.googleusercontent.com"
+        buttonText="Login with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
